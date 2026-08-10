224f91fecc27b2a95cdad330c4c8984b2420e601
joon-99 <6067938+Joon-99@users.noreply.github.com>
2026-08-10 03:45:42 +0200

    refactoring CartController

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -2,12 +2,10 @@
 
 namespace App\Controller;
 
-use App\Entity\CartItem;
 use App\Entity\Product;
 use App\Entity\User;
-use App\Repository\CartItemRepository;
 use App\Service\CartService;
-use Doctrine\ORM\EntityManagerInterface;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -19,16 +17,21 @@ final class CartController extends AbstractController
 {
     public function __construct(
         private readonly CartService $cartService,
-        private readonly EntityManagerInterface $entityManager,
-        private readonly CartItemRepository $cartItemRepo,)
-    {
+        private readonly LoggerInterface $logger,
+    ) {
     }
 
     #[Route('/cart', name: 'app_cart')]
     public function index(#[CurrentUser] User $user): Response
     {
-        $cart = $this->cartService->getOrCreateCart($user);
-        $cartItems = $cart->getCartItems();
+        $cart = null;
+        try {
+            $cart = $this->cartService->getOrCreateCart($user);
+        } catch (\Exception $e) {
+            $this->logger->error(__METHOD__ . "Error while fetching the cart: " . $e->getMessage());
+            $this->addFlash('error', "Une erreur est survenue lors de la récupération du panier.");
+        }
+        $cartItems = $cart ? $cart->getCartItems() : [];
 
         return $this->render('cart/index.html.twig', [
             'cart' => $cart,
@@ -37,24 +40,13 @@ final class CartController extends AbstractController
     }
 
     #[Route('/cart/add/{product}', name: 'app_cart_add')]
-    public function add(Product $product, #[CurrentUser] User $user): Response
+    public function add(Product $product, #[CurrentUser] User $user, int $quantity = 1): Response
     {
-        //TODO refactor this into a service
-        $cart = $this->cartService->getOrCreateCart($user);
-        $cartItem = $this->cartItemRepo->findFromCartByProduct($cart, $product);
-        if ($cartItem) {
-            $cartItem->setQuantity($cartItem->getQuantity() + 1);
-        } else {
-            $cartItem = new CartItem($cart, 1, $product);
-            $cart->addCartItem($cartItem);
-        }
-
         try {
-            $this->entityManager->persist($cart);
-            $this->entityManager->persist($cartItem);
-            $this->entityManager->flush();
+            $this->cartService->addItemToCart($user, $product, $quantity);
             $this->addFlash('success', "Le produit a été ajouté au panier.");
         } catch (\Exception $e) {
+            $this->logger->error(__METHOD__ . "Error while adding an item to the cart: " . $e->getMessage());
             $this->addFlash('error', "Une erreur est survenue lors de l'ajout du produit au panier.");
         }
 
--- src/Service/CartService.php
+++ src/Service/CartService.php
@@ -4,14 +4,24 @@ namespace App\Service;
 
 use App\Entity\Cart;
 use App\Entity\User;
+use App\Entity\Product;
+use App\Entity\CartItem;
 use Doctrine\ORM\EntityManagerInterface;
+use App\Repository\CartItemRepository;
 
 final class CartService
 {
-    public function __construct(private readonly EntityManagerInterface $entityManager)
+    public function __construct(
+        private readonly EntityManagerInterface $entityManager,
+        private readonly CartItemRepository $cartItemRepo,)
     {
     }
 
+    /**
+     * Get the user's cart or create a new one if it doesn't exist.
+     * 
+     * @throws \Exception
+     */
     public function getOrCreateCart(User $user): Cart
     {
         $cart = $user->getCart();
@@ -25,5 +35,26 @@ final class CartService
         $this->entityManager->flush();
 
         return $cart;
+    }
+
+    /**
+     * Add an item to the user's cart. If the item already exists in the cart, increase its quantity.
+     * 
+     * @throws \Exception
+     */
+    public function addItemToCart(User $user, Product $product, int $quantity = 1): void
+    {
+        $cart = $this->getOrCreateCart($user);
+        $cartItem = $this->cartItemRepo->findFromCartByProduct($cart, $product);
+
+        if ($cartItem) {
+            $cartItem->setQuantity($cartItem->getQuantity() + $quantity);
+        } else {
+            $cartItem = new CartItem($cart, $quantity, $product);
+            $cart->addCartItem($cartItem);
+        }
+
+        $this->entityManager->persist($cartItem);
+        $this->entityManager->flush();
     }
 }
